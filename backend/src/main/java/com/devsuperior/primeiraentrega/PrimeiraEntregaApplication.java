5610418a5e54d2ccfe3760dee0fe48f1a6643f7e
Renan Soares <renansoaresffc@gmail.com>
2021-10-13 16:32:58 -0300

    First commit

---
+++ backend/src/main/java/com/devsuperior/primeiraentrega/PrimeiraEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.primeiraentrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraEntregaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/primeiraentrega/PrimeiraEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.primeiraentrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
