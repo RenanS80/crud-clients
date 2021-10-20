534f2f6ec2c0f5b11a94db7bd2318941da566fa4
Renan Soares <renansoaresffc@gmail.com>
2021-10-20 19:27:51 -0300

    Client DTO

--- backend/src/main/java/com/devsuperior/primeiraentrega/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/primeiraentrega/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.primeiraentrega.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.primeiraentrega.dto.ClientDTO;
 import com.devsuperior.primeiraentrega.entities.Client;
 import com.devsuperior.primeiraentrega.repositories.ClientRepository;
 
@@ -15,8 +17,10 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	// Retorna todos os clientes
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
-		return repository.findAll();
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 }
