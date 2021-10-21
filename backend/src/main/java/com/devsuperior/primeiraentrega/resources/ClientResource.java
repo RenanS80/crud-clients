b9da91c85019b1b3e5d847c99d74a2336b2fbbcd
Renan Soares <renansoaresffc@gmail.com>
2021-10-21 16:56:24 -0300

    PUT Client

--- backend/src/main/java/com/devsuperior/primeiraentrega/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/primeiraentrega/resources/ClientResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,12 +36,20 @@ public class ClientResource {
 		ClientDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
-		
+	
+	// Endpoint para inserir um cliente
 	@PostMapping
 	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	// Endpoint para atualizar um registro de cliente
+	@PutMapping(value = "{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- backend/src/main/java/com/devsuperior/primeiraentrega/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/primeiraentrega/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.primeiraentrega.dto.ClientDTO;
 import com.devsuperior.primeiraentrega.entities.Client;
 import com.devsuperior.primeiraentrega.repositories.ClientRepository;
-import com.devsuperior.primeiraentrega.services.exceptions.EntityNotFoundException;
+import com.devsuperior.primeiraentrega.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -46,5 +48,25 @@ public class ClientService {
 		
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+	}
+	
+	// Atualiza um cliente no banco
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" +id);
+		}
+		
 	}
 }
